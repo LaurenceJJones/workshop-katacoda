c80fe0c7032745cb2fddc195a164bf50ba93b4cb
laurence <laurence.jones@live.co.uk>
2023-01-05 21:23:25 +0000

    Add fake machineid

--- crowdsec-setup/env-init.sh
+++ crowdsec-setup/env-init.sh
@@ -1,5 +1,5 @@
 # Install nginx service for CrowdSec auto detection
-ssh root@host01 "apt update && apt install nginx -y"
+ssh root@host01 "echo 'githubciXXXXXXXXXXXXXXXXXXXXXXXX' > /etc/machine-id && apt update && apt install nginx -y"
 
 # Install hydra package for attacker demo
 ssh root@host02 "apt update && apt install hydra -y"
