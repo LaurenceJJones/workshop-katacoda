c466613a078e2aed2afe8b8b29b0e658677cde3f
laurence <laurence.jones@live.co.uk>
2023-01-09 20:31:00 +0000

    Lets try

--- crowdsec-parsers-scenarios/env-init.sh
+++ crowdsec-parsers-scenarios/env-init.sh
@@ -1,7 +1,11 @@
 echo 'githubciXXXXXXXXXXXXXXXXXXXXXXXX' > /etc/machine-id
-apt update && apt upgrade -y
 curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | bash
 apt install crowdsec -y
 echo "db_config:
   use_wal: true" > /etc/crowdsec/config.yaml.local
 systemctl enable --now crowdsec.service
+cd /opt/ && git clone https://github.com/LaurenceJJones/workshop-katacoda.git
+cd workshop-katacoda/crowdsec-parsers-scenarios/assets/myapp/
+go mod download
+go build .
+./myapp
