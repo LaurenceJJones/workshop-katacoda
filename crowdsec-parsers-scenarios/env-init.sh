464ff681f6414c2cf0051f065d3bf4e53f59feb5
laurence <laurence.jones@live.co.uk>
2023-01-09 19:19:42 +0000

    Enable crowdsec and start after install

--- crowdsec-parsers-scenarios/env-init.sh
+++ crowdsec-parsers-scenarios/env-init.sh
@@ -2,3 +2,4 @@ echo 'githubciXXXXXXXXXXXXXXXXXXXXXXXX' > /etc/machine-id
 apt update && apt upgrade -y
 curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | bash
 apt install crowdsec -y
+systemctl enable --now crowdsec.service
