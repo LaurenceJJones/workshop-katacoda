383dcb62b22434e71664ad612d965fd33625c786
laurence <laurence.jones@live.co.uk>
2023-01-09 22:24:48 +0000

    Update and add admin page if they guess user/pass

--- crowdsec-parsers-scenarios/assets/myapp/main.go
+++ crowdsec-parsers-scenarios/assets/myapp/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -13,8 +14,6 @@ import (
 
 var password, user string
 
-var randomKey = []byte("sup3rs3cr3t")
-
 func main() {
 	flag.StringVar(&user, "user", "user", "User arg")
 	flag.StringVar(&password, "password", "password", "Password arg")
@@ -22,66 +21,93 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
 
-	router.Use(sessions.Sessions("session", cookie.NewStore(randomKey)))
+	router.Use(sessions.Sessions("session", cookie.NewStore([]byte("sup3rs3cr3t"))))
 
 	i := router.Group("/")
 	{
-		i.GET("/", LoginGetHandler())
-		i.POST("/", LoginPostHandler())
-	}
+		i.GET("/", func(c *gin.Context) {
+			session := sessions.Default(c)
+			user := session.Get("user")
+			if user != nil {
+				c.HTML(http.StatusBadRequest, "index.html",
+					gin.H{
+						"content": "Please logout first",
+						"user":    user,
+					})
+				return
+			}
+			c.HTML(http.StatusOK, "index.html", gin.H{
+				"content": "",
+				"user":    user,
+			})
+		})
+		i.POST("/", func(c *gin.Context) {
+			session := sessions.Default(c)
+			user := session.Get("user")
+			if user != nil {
+				c.HTML(http.StatusBadRequest, "index.html", gin.H{"content": "Please logout first"})
+				return
+			}
 
-	router.Run("0.0.0.0:3000")
-}
+			u := c.PostForm("username")
+			p := c.PostForm("password")
 
-func LoginPostHandler() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		session := sessions.Default(c)
-		user := session.Get(randomKey)
-		if user != nil {
-			c.HTML(http.StatusBadRequest, "login.html", gin.H{"content": "Please logout first"})
-			return
-		}
+			if EmptyUserPass(u, p) {
+				c.HTML(http.StatusBadRequest, "index.html", gin.H{"content": "Parameters can't be empty"})
+				return
+			}
 
-		u := c.PostForm("username")
-		p := c.PostForm("password")
+			if !CheckUserPass(u, p) {
+				c.HTML(http.StatusUnauthorized, "index.html", gin.H{"content": "Incorrect username or password"})
+				return
+			}
 
-		if EmptyUserPass(u, p) {
-			c.HTML(http.StatusBadRequest, "index.html", gin.H{"content": "Parameters can't be empty"})
-			return
-		}
+			session.Set("user", u)
+			if err := session.Save(); err != nil {
+				c.HTML(http.StatusInternalServerError, "index.html", gin.H{"content": "Failed to save session"})
+				return
+			}
 
-		if CheckUserPass(u, p) {
-			c.HTML(http.StatusUnauthorized, "index.html", gin.H{"content": "Incorrect username or password"})
-			return
-		}
-
-		session.Set(randomKey, u)
-		if err := session.Save(); err != nil {
-			c.HTML(http.StatusInternalServerError, "index.html", gin.H{"content": "Failed to save session"})
-			return
-		}
-
-		//c.Redirect(http.StatusMovedPermanently, "/dashboard")
-	}
-}
-
-func LoginGetHandler() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		session := sessions.Default(c)
-		user := session.Get(randomKey)
-		if user != nil {
-			c.HTML(http.StatusBadRequest, "index.html",
-				gin.H{
-					"content": "Please logout first",
-					"user":    user,
-				})
-			return
-		}
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"content": "",
-			"user":    user,
+			c.Redirect(http.StatusMovedPermanently, "/admin")
 		})
 	}
+	p := router.Group("/")
+	{
+		p.Use(func(c *gin.Context) {
+			session := sessions.Default(c)
+			user := session.Get("user")
+			fmt.Println("Auth middleware", user)
+			if user == nil {
+				c.Redirect(http.StatusMovedPermanently, "/")
+				c.Abort()
+				return
+			}
+			c.Next()
+		})
+		p.GET("/admin", func(c *gin.Context) {
+			session := sessions.Default(c)
+			user := session.Get("user")
+			c.HTML(http.StatusOK, "admin.html", gin.H{
+				"content": "This is a dashboard",
+				"user":    user,
+			})
+		})
+		p.GET("/logout", func(c *gin.Context) {
+			session := sessions.Default(c)
+			user := session.Get("user")
+			fmt.Println("Trying to logout")
+			if user == nil {
+				return
+			}
+			session.Delete("user")
+			if err := session.Save(); err != nil {
+				return
+			}
+
+			c.Redirect(http.StatusMovedPermanently, "/")
+		})
+	}
+	router.Run("0.0.0.0:3000")
 }
 
 func CheckUserPass(u, p string) bool {
