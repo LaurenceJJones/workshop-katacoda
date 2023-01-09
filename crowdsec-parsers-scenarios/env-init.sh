c62e021fc81d63082f2a6e11a465b04292cadd8d
laurence <laurence.jones@live.co.uk>
2023-01-09 19:33:14 +0000

    Auto wal mode just because

--- crowdsec-parsers-scenarios/env-init.sh
+++ crowdsec-parsers-scenarios/env-init.sh
@@ -2,4 +2,6 @@ echo 'githubciXXXXXXXXXXXXXXXXXXXXXXXX' > /etc/machine-id
 apt update && apt upgrade -y
 curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | bash
 apt install crowdsec -y
+echo "db_config:
+  use_wal: true" > /etc/crowdsec/config.yaml.local
 systemctl enable --now crowdsec.service
