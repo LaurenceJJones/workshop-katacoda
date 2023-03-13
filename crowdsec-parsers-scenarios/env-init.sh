56cb2a93732058a5343340816b369040c68c59ae
Laurence <laurence.jones@live.co.uk>
2023-03-13 11:59:31 +0000

    Export the environ so it persists

--- crowdsec-parsers-scenarios/env-init.sh
+++ crowdsec-parsers-scenarios/env-init.sh
@@ -16,6 +16,6 @@ cd /opt/ && git clone https://github.com/LaurenceJJones/workshop-katacoda.git
 cd workshop-katacoda/crowdsec-parsers-scenarios/assets/myapp/
 /usr/local/go/bin/go mod download
 /usr/local/go/bin/go build .
-MYAPP_PASSWORD=$(openssl rand -base64 12)
-MYAPP_USER=$(shuf -n 1 users.txt)
+export MYAPP_PASSWORD=$(openssl rand -base64 12)
+export MYAPP_USER=$(shuf -n 1 users.txt)
 ./myapp
