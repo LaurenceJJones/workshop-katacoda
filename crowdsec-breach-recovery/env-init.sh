3ab40fde863d548c67620b2abcf873d9dc46c6d0
Laurence <laurence.jones@live.co.uk>
2023-07-11 21:59:27 +0100

    Fix passwords

--- crowdsec-breach-recovery/env-init.sh
+++ crowdsec-breach-recovery/env-init.sh
@@ -3,7 +3,7 @@
 # setup elk stack
 wget -qO- https://github.com/deviantony/docker-elk/archive/refs/tags/8.2302.1.tar.gz | tar xvz
 cd docker-elk*
-sed -i "s/''/'crowdsec'/g" .env
-sed -i "s/changeme/crowdsec/g" .env
+sed -i "s/''/crowdsec/g" .env
+sed -i "s/'changeme'/crowdsec/g" .env
 docker-compose up setup
 docker-compose up -d
