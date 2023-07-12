75de41bee1ea1827e9574f697eb151d73d65867f
Laurence <laurence.jones@live.co.uk>
2023-07-12 10:28:50 +0100

    Attempts to fix

--- crowdsec-parsers-scenarios/step1-verify.sh
+++ crowdsec-parsers-scenarios/step1-verify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s http://127.0.0.1:8080/health | grep -q status || echo "CrowdSec is not running, please wait a few seconds and try again." && exit 1
+curl -s http://127.0.0.1:8080/health | grep -q status
