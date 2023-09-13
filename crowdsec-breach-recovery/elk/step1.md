29534d1d3b6ea5e3a8ca0e32afb882b9e248dfed
Laurence <laurence.jones@live.co.uk>
2023-09-13 14:53:07 +0100

    Add iptables fake logs

--- crowdsec-breach-recovery/elk/step1.md
+++ crowdsec-breach-recovery/elk/step1.md
@@ -1,8 +1,8 @@
 ## Introduction to the breach
 
-A company has reached out to us after they have been contact by a hacker group that claims to have breached their network and stolen confidential data. They have provided the company with a sample of the data they have stolen to show it is legitimate. We need to investigate the breach and determine the extent of the damage.
+A Marys Farm has reached out to us after they have been contact by a hacker group that claims to have breached their network and stolen confidential data. They have provided the company with a sample of the data they have stolen to show it is legitimate. We need to investigate the breach and determine the extent of the damage.
 
-The company does not know how the hackers got in, but they have provided us logs from their web server, firewall and databases. We will need to analyze these logs to determine how the hackers got in and what they did.
+The Marys Farm does not know how the hackers got in, but they have provided us logs from their web server, firewall and databases. We will need to analyze these logs to determine how the hackers got in and what they did.
 
 We will be using the ELK stack to analyze the logs. The ELK stack is a collection of open source tools that are commonly used for manual log analysis. The ELK stack consists of Elasticsearch, Logstash, and Kibana. Elasticsearch is a search engine that is used to store and search logs. Logstash is a tool that is used to collect and parse logs. Kibana is a web interface that is used to visualize logs.
 
@@ -15,3 +15,5 @@ The ELK stack has been setup for us and is available at the following [URL]({{TR
 The ELK stack has been configured with the following credentials:
 - user: elastic
 - password: crowdsec
+
+**The setup can take sometime so if you get 502 gateway errors, please wait a few minutes and try again.**
--- crowdsec-breach-recovery/env-init.sh
+++ crowdsec-breach-recovery/env-init.sh
@@ -32,6 +32,7 @@ filebeat.config:
 
 filebeat modules enable nginx
 filebeat modules enable system
+filebeat modules enable iptables
 
 cat <<-EOT > "/etc/filebeat/modules.d/nginx.yml"
 - module: nginx
@@ -48,29 +49,56 @@ cat <<-EOT > "/etc/filebeat/modules.d/system.yml"
     enabled: true
     var.paths: ["/var/log/auth.log*"]
 	EOT
-
+cat <<-EOT > "/etc/filebeat/modules.d/iptables.yml"
+- module: iptables
+  log:
+    enabled: true
+    var.paths: ["/var/log/iptables.log"]
+    var.input: "file"
+	EOT
 sleep 60
 filebeat setup -e
 systemctl enable --now filebeat
 cd - || exit 1
+
+## Generate fake logs :D
 git clone https://github.com/punk-security/pwnspoof
 cd pwnspoof || exit 1
 python pwnspoof.py wordpress --session-count 7000 --log-start-date "$(date -d '-30 days' +%Y%m%d)" --log-end-date "$(date +%Y%m%d)"  --spoofed-attacks 0 --attack-type command_injection --server-fqdn marysfarm.local --out /var/log/pwn.log --additional-attacker-ips 14.32.0.74,221.210.252.36,204.157.240.55,200.59.184.155,103.231.177.154 --server-type NGINX
+## Use pwnspoof for nginx logs
 
 min=34000
 max=36000
 NUM=0
+## Grep malicious IP's out of the pwn.log and inject them into the other IOCS
 for i in $(grep "?cmd" /var/log/pwn.log | cut -d ' ' -f1 | sort -u); do
   NUM=$((NUM-5))
   PORT=$((min + RANDOM % max))
   compdate=$(date -d "$NUM days" "+%b  %-d %H:%M:%S")
+  ## Inject ssh IOCS
   echo "$compdate bullseye sshd[557]: Accepted password for root from $i port $PORT ssh2" >> /var/log/auth.log
+  MIN_NUM=0
+  ## Loop over common ssh ports
+  for port in 22 2022 2222 2202 2002 2000 2220 202 220; do
+    MIN_NUM=$((NUM-10))
+    compdate=$(date -d "$MIN_NUM seconds" "+%b  %-d %H:%M:%S")
+    ## Inject iptables IOCS
+    if [ $port -eq 22 ]; then
+      ## Inject accepted
+      echo "$compdate bullseye kernel: [  659.418604] IN=eth0 OUT= MAC=52:54:00:83:ad:a5:52:54:00:99:c9:e0:08:00 SRC=$i DST=1.2.3.4 LEN=60 TOS=0x00 PREC=0x00 TTL=64 ID=54729 DF PROTO=TCP SPT=41894 DPT=$port WINDOW=64240 RES=0x00 SYN URGP=0 ACCEPT" >> /var/log/iptables.log
+    else
+      ## Inject dropped
+      echo "$compdate bullseye kernel: [  659.418604] IN=eth0 OUT= MAC=52:54:00:83:ad:a5:52:54:00:99:c9:e0:08:00 SRC=$i DST=1.2.3.4 LEN=60 TOS=0x00 PREC=0x00 TTL=64 ID=54729 DF PROTO=TCP SPT=41894 DPT=$port WINDOW=64240 RES=0x00 SYN URGP=0" >> /var/log/iptables.log
+    fi
+  done
 done
 
+
+## Inject "legit" logins for a constant IP
 NUM=0
-for i in {1..1000}; do
+for i in {1..100}; do
   NUM=$((NUM-i))
   PORT=$((min + RANDOM % max))
   compdate=$(date -d "$NUM days" "+%b  %-d %H:%M:%S")
-  echo "$compdate bullseye sshd[557]: Accepted password for webdev from 1.2.3.4 port $PORT ssh2" >> /var/log/auth.log
+  echo "$compdate bullseye sshd[557]: Accepted password for root from 1.2.3.4 port $PORT ssh2" >> /var/log/auth.log
 done
