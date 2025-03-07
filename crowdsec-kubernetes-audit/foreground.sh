826f75e5a1d77e19efeffe1aa60318ce53343d58
Sebastien Blot <sebastien@crowdsec.net>
2025-03-07 17:17:05 +0100

    add a more recent yq

--- crowdsec-kubernetes-audit/foreground.sh
+++ crowdsec-kubernetes-audit/foreground.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "Downloading yq"
+
+wget https://github.com/mikefarah/yq/releases/download/v4.45.1/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq
+
 echo "Updating api-server configuration to enable audit logging"
 
 yq -e -i  '.spec.containers[0].command += "--audit-policy-file=/etc/kubernetes/manifests/audit-policy.yaml"' /etc/kubernetes/manifests/kube-apiserver.yaml
