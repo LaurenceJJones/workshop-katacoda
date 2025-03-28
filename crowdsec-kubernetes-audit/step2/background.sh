3b128b2ac494f45430ec9bf5b5fe3acffaa2f757
Sebastien Blot <sebastien@crowdsec.net>
2025-03-28 14:15:36 +0100

    up

--- crowdsec-kubernetes-audit/step2/background.sh
+++ crowdsec-kubernetes-audit/step2/background.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-kubectl wait --for=condition=Ready $(kubectl -n crowdsec get pods -l type=agent -o jsonpath='{.items[0].metadata.name}')
+kubectl -n crowdsec wait --for=condition=Ready pod $(kubectl -n crowdsec get pods -l type=agent -o jsonpath='{.items[0].metadata.name}')
+touch /root/done
