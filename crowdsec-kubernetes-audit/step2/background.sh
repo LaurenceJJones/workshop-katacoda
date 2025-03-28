20e17207e086262975c318f4519f15d3b69ebc70
Sebastien Blot <sebastien@crowdsec.net>
2025-03-28 15:33:32 +0100

    up

--- crowdsec-kubernetes-audit/step2/background.sh
+++ crowdsec-kubernetes-audit/step2/background.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kubectl -n crowdsec wait --for=condition=Ready pod $(kubectl -n crowdsec get pods -l type=agent -o jsonpath='{.items[0].metadata.name}')
+kubectl -n crowdsec wait --for=condition=Ready --timeout=300s pod $(kubectl -n crowdsec get pods -l type=agent -o jsonpath='{.items[0].metadata.name}') 
 touch /root/done
--- crowdsec-kubernetes-audit/step2/foreground.sh
+++ crowdsec-kubernetes-audit/step2/foreground.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # waiting for the cluster to be ready
-echo "Waiting for crowdsec deployment to be ready..."
+echo "Waiting for crowdsec deployment to be ready... (this may take a few minutes)"
 while [ ! -f /root/done ]; do sleep 2; done
 echo "crowdsec deployment is ready"
 
